afad08c551f6d934d9fae0a8e709fc2adb88a011
ddorn <diego.dorn@free.fr>
2020-02-01 15:41:18 +0100

    paddle: speed specified in config

--- src/systems/paddle.rs
+++ src/systems/paddle.rs
@@ -1,11 +1,11 @@
 
-use amethyst::core::{Transform, SystemDesc};
+use amethyst::core::{Transform, SystemDesc, timing::Time};
 use amethyst::derive::SystemDesc;
 use amethyst::ecs::{Join, Read, ReadStorage, System, SystemData, World, WriteStorage};
 use amethyst::input::{InputHandler, StringBindings};
 
 use crate::components::{Paddle, Side};
-use crate::config::ArenaConfig;
+use crate::config::{ArenaConfig, PaddleConfig};
 
 #[derive(SystemDesc)]
 pub struct PaddleSystem;
@@ -13,6 +13,8 @@ pub struct PaddleSystem;
 impl<'s> System<'s> for PaddleSystem {
     type SystemData = (
         Read<'s, ArenaConfig>,
+        Read<'s, PaddleConfig>,
+        Read<'s, Time>,
         WriteStorage<'s, Transform>,
         ReadStorage<'s, Paddle>,
         Read<'s, InputHandler<StringBindings>>,
@@ -20,6 +22,8 @@ impl<'s> System<'s> for PaddleSystem {
 
     fn run(&mut self, data: Self::SystemData) {
         let (arena,
+            paddles_config,
+            time,
             mut transforms,
             paddles,
             input) = data;
@@ -31,7 +35,7 @@ impl<'s> System<'s> for PaddleSystem {
             };
 
             if let Some(mv_amount) = movement {
-                let mv_amount = 3.0 * mv_amount;
+                let mv_amount = mv_amount * paddles_config.speed * time.delta_seconds();
                 if mv_amount != 0.0 {
                     let y = transform.translation().y;
 
