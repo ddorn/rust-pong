66d62ccbf7c697f9e3cc92668ed1b4639e9aeb35
ddorn <diego.dorn@free.fr>
2020-02-01 20:50:12 +0100

    bounce angle depends on hit position

--- src/systems/bounce_balls.rs
+++ src/systems/bounce_balls.rs
@@ -1,8 +1,9 @@
-use amethyst::core::{Transform, SystemDesc};
+use amethyst::core::{Transform, SystemDesc, math::Vector2};
 use amethyst::derive::SystemDesc;
 use amethyst::ecs::{Join, Read, ReadStorage, System, SystemData, World, WriteStorage};
 use crate::components::{Ball, Paddle, Side};
 use crate::config::{ArenaConfig, BallConfig};
+use std::f32::consts::PI;
 
 #[derive(SystemDesc)]
 pub struct BounceBallsSystem;
@@ -28,9 +29,9 @@ impl<'s> System<'s> for BounceBallsSystem {
             let y: f32 = transform.translation().y;
             let r: f32 = ball.radius;
 
+            // Bounce against top and bottom walls
             if (y < r && ball.direction[1] < 0.0)
-                || (y > arena.height - r && ball.direction[1] > 0.0)
-            {
+                || (y > arena.height - r && ball.direction[1] > 0.0) {
                 ball.direction[1] *= -1.0;
             }
 
@@ -52,8 +53,20 @@ impl<'s> System<'s> for BounceBallsSystem {
                 ) {
                     if (paddle.side == Side::Left && ball.direction[0] < 0.0)
                         || (paddle.side == Side::Right && ball.direction[0] > 0.0) {
-                        // Reverse the x direction after bounce
-                        ball.direction[0] *= -1.0;
+
+                        // We make the ball bounce with a different angle depending where it landed
+                        let hit_prop = ((y - paddle_y) / paddle.height + 0.5)
+                            .min(1.0)  // Clamp
+                            .max(0.0);
+
+                        // The new angle is between -45 and 45 degrees for each bat
+                        let a = PI / 4.0;
+                        let slope = match paddle.side {
+                            Side::Left => lerp(hit_prop, -a, a),
+                            Side::Right => lerp(hit_prop, PI + a, PI - a),
+                        };
+                        ball.direction = Vector2::new(slope.cos(), slope.sin());
+
                         // And accelerate the ball linearly
                         ball.speed += balls_config.bounce_acceleration;
                         // Cap the speed though
@@ -61,7 +74,6 @@ impl<'s> System<'s> for BounceBallsSystem {
                     }
                 }
             }
-
         }
     }
 }
@@ -69,4 +81,10 @@ impl<'s> System<'s> for BounceBallsSystem {
 /// Whether a point is included in a rectangle or on its border
 fn point_in_rect(x: f32, y: f32, left: f32, bottom: f32, right: f32, top: f32) -> bool {
     left <= x && x <= right && bottom <= y && y <= top
+}
+
+/// Linear interpolation between a and b.
+/// Return a when t is 0 and b when t is 1.
+fn lerp(t: f32, a: f32, b: f32) -> f32 {
+    (1.0 - t) * a + t * b
 }
