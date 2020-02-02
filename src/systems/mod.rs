ef98d2a13072d2987b640e19a62ab58dc22a3707
ddorn <diego.dorn@free.fr>
2020-02-02 15:20:24 +0100

    more prcise name

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -1,13 +1,13 @@
 pub use self::paddle::PaddleSystem;
 pub use self::winner::WinnerSystem;
 pub use self::wall_bounce::WallBounceSystem;
-pub use self::move_balls::MoveStraightSystem;
+pub use self::move_straight::MoveStraightSystem;
 pub use self::paddle_bounce::PaddleBounceSystem;
 pub use self::sound_effects::SoundEffectsSystem;
 
 mod paddle;
 mod winner;
-mod move_balls;
+mod move_straight;
 mod paddle_bounce;
 mod sound_effects;
 mod wall_bounce;
